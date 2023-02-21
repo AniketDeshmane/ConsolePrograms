fea6f84f480435bff1465cf6ea9a661af60564c1
amd_404 <aniketmdeshmane@gmail.com>
2023-02-22 00:10:32 +0530

    Added files

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<char,int> myDict = new Dictionary<char,int>();
+
+            string name = Console.ReadLine();
+
+            for (int i = 0; i<name.Length; i++)
+            {
+                if (myDict.ContainsKey(name[i]))
+                {
+                    myDict[name[i]] += 1;
+                }
+                else
+                {
+                    myDict.Add(name[i], 1);
+                }
+               
+            }
+
+
+
+            for (int i = 0; i < myDict.Count; i++)
+            {
+                Console.WriteLine("Letter: {0}, Value: {1}",
+                                                        myDict.ElementAt(i).Key,
+                                                        myDict.ElementAt(i).Value);
+            }
+            Console.ReadLine();
+        }
+    }
+}
